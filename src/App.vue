cec49e670e1903e6568d91b0b419b1d0c56977a8
A799881 <aleksandra.swierczek@atos.net>
2021-10-05 16:23:58 +0200

    login in navigation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>|
-
-      <!-- NEW - add a route to the profile page -->
-      <router-link v-if="$auth.isAuthenticated" to="/profile">Profile</router-link>
-    </div>
+    <Navbar/>
     <router-view />
   </div>
 </template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default{
+  name: "app",
+  components: {
+    Navbar
+  }
+}
+</script>
 
 <style>
 #app {
@@ -18,18 +22,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
